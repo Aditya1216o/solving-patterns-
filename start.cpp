8e05f49c27478bdb1945b83a318403e7efa637ba
Aditya <addityadesai00034@gmail.com>
2026-06-02 19:30:00 +0530

    inverse pyramid

--- start.cpp
+++ start.cpp
@@ -41,9 +41,46 @@ void print5(int n ) {
     cout << endl;
   }
 }
+void print6(int n ) {
+     for (int i = 1; i <= n; i++) {
+    for(int j = 1; j <= n-i+1; j++) {
+        cout << j << ""  ;
+    }
+    cout << endl;
+  }
+}
+void print7(int n ) {
+     for (int i = 1; i <= n; i++) {
+    for(int j = 1; j <= n-i; j++) {
+        cout << " "  ;
+    }
+    for(int j = 1; j <= 2*i-1; j++) {
+        cout << "*"  ;
+    }
+    for(int j = 1; j <= n-i; j++) {
+        cout << " "  ;
+    }
+    cout << endl;
+  }
+}
+void print8(int n ) {
+     for (int i = 0; i <= n; i++) {
+    for(int j = 0; j < i; j++) {
+        cout << " "  ;
+    }
+    for(int j = 0; j < 2*n-(2*i +1); j++) {
+        cout << "*"  ;
+    }
+    for(int j = 0; j < i; j++) {
+        cout << " "  ;
+    }
+    cout << endl;
+  }
+}
+
 int main() {
     int n;
     cin >> n;
   
-  print5(n);
+  print8(n);
           }
