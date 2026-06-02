552dfdd3df56fbb3c09dcb1e729b3f94df4e54a7
Aditya <addityadesai00034@gmail.com>
2026-06-03 00:47:54 +0530

    15 patterns completed

--- start.cpp
+++ start.cpp
@@ -125,9 +125,58 @@ void print10(int n ) {
     cout << endl;
   }
  }
+  void print12(int n ){
+  int space  = 2*(n-1);
+  for(int i =0; i < n ; i++){
+    for(int j =1; j<=i; j++){
+      cout << j;
+    }
+    for(int j =1 ; j <= space ; j++){
+      cout << " ";
+
+    }
+     for(int j =i; j>=1; j--){
+      cout << j;
+    }
+    cout << endl;
+    space -=2;
+  }
+ }
+ void print13(int n){
+  int num = 1;
+
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++){
+      cout <<  num << " ";
+      num= num +1;
+
+
+    }
+    cout <<  endl;
+    
+  }
+ }
+ void print14(int n){
+  for(int i=0; i<n; i++){
+    for(char c = 'A'; c<= 'A'+i; c++) {
+      cout << c << " ";
+
+    }
+    cout << endl;
+  }
+ }
+  void print15(int n){
+  for(int i=0; i<n; i++){
+    for(char c = 'A'; c<= 'A'+(n-i-1); c++) {
+      cout << c << " ";
+
+    }
+    cout << endl;
+  }
+ }
 int main() {
     int n;
     cin >> n;
   
-  print11(n);
+  print15(n);
           }
