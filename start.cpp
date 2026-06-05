1281417a825742393c4f4fda46d61c3ec49c1dcf
Aditya <addityadesai00034@gmail.com>
2026-06-05 23:06:21 +0530

    17 patterns completed

--- start.cpp
+++ start.cpp
@@ -174,9 +174,18 @@ void print10(int n ) {
     cout << endl;
   }
  }
+ void print16(int n ) {
+  for(int i = 0 ; i < n ; i++){
+    char ch = 'A' + i ;
+    for(int j = 0 ; j <= i ; j++){
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+ }
 int main() {
     int n;
     cin >> n;
   
-  print15(n);
+  print16(n);
           }
